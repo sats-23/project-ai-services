0bf644b2af50a4ef1b516017e2a267b47b078c8e
Sathvik <Sathvik.S@ibm.com>
2026-02-24 14:06:12 +0530

    Implement app delete for openshift runtime
    Signed-off-by: Sathvik <Sathvik.S@ibm.com>

--- ai-services/internal/pkg/application/openshift/delete.go
+++ ai-services/internal/pkg/application/openshift/delete.go
@@ -1,13 +1,67 @@
 package openshift
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/project-ai-services/ai-services/internal/pkg/application/types"
+	"github.com/project-ai-services/ai-services/internal/pkg/helm"
 	"github.com/project-ai-services/ai-services/internal/pkg/logger"
+	"github.com/project-ai-services/ai-services/internal/pkg/spinner"
+	"github.com/project-ai-services/ai-services/internal/pkg/utils"
 )
 
 // Delete removes an application and its associated resources.
 func (o *OpenshiftApplication) Delete(opts types.DeleteOptions) error {
-	logger.Warningln("not implemented")
+	app := opts.Name
+	namespace := app
+
+	// Create a new Helm client
+	helmClient, err := helm.NewHelm(namespace)
+	if err != nil {
+		return fmt.Errorf("failed to create helm client: %w", err)
+	}
+
+	// Check if the app exists
+	isAppExist, err := helmClient.IsReleaseExist(app)
+	if err != nil {
+		return fmt.Errorf("failed to check if application exists: %w", err)
+	}
+
+	if !isAppExist {
+		logger.Infof("Application '%s' does not exist in namespace '%s'\n", app, namespace)
+
+		return nil
+	}
+
+	if !opts.AutoYes {
+		confirmDelete, err := utils.ConfirmAction("Are you sure you want to delete the application '" + app + "' from namespace '" + namespace + "'?")
+		if err != nil {
+			return fmt.Errorf("failed to take user input: %w", err)
+		}
+		if !confirmDelete {
+			logger.Infoln("Deletion cancelled")
+
+			return nil
+		}
+	}
+
+	logger.Infoln("Proceeding with deletion...")
+
+	ctx := context.Background()
+	s := spinner.New("Deleting application '" + app + "'...")
+
+	s.Start(ctx)
+
+	// Perform helm uninstall
+	err = helmClient.Uninstall(app, nil)
+	if err != nil {
+		s.Fail("failed to delete application")
+
+		return fmt.Errorf("failed to perform app uninstallation: %w", err)
+	}
+
+	s.Stop("Application '" + app + "' deleted successfully")
 
 	return nil
 }
--- ai-services/internal/pkg/helm/helm.go
+++ ai-services/internal/pkg/helm/helm.go
@@ -107,3 +107,24 @@ func (h *Helm) IsReleaseExist(release string) (bool, error) {
 
 	return true, nil
 }
+
+type UninstallOpts struct {
+	Timeout time.Duration
+}
+
+func (h *Helm) Uninstall(release string, opts *UninstallOpts) error {
+	// Configure the Uninstall client
+	uninstallClient := action.NewUninstall(h.actionConfig)
+	uninstallClient.WaitStrategy = kube.StatusWatcherStrategy
+	if opts != nil {
+		uninstallClient.Timeout = opts.Timeout
+	}
+
+	// Perform helm uninstall
+	_, err := uninstallClient.Run(release)
+	if err != nil {
+		return fmt.Errorf("Uninstall failed: %w", err)
+	}
+
+	return nil
+}
